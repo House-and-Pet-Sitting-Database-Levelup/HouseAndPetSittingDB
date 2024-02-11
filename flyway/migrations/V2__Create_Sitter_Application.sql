a0e482dd1912a234be858b1454279d9b766f7dc4
Lior-Becker <lior.becker@bbd.co.za>
2024-02-11 15:40:21 +0200

    added sql code

---
+++ flyway/migrations/V2__Create_Sitter_Application.sql
@@ -0,0 +1,17 @@
+CREATE TABLE SitterApplications(
+	PK_ApplicationID uniqueidentifier PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
+	FK_BookingID uniqueidentifier NOT NULL,
+	FK_UserID uniqueidentifier NOT NULL,
+	Status varchar(8),
+	CONSTRAINT chk_Status CHECK (Status IN ('Pending','Approved', 'Rejected'))
+);
+
+ALTER TABLE SitterApplications
+	ADD CONSTRAINT fk_BookingReference
+	FOREIGN KEY (FK_BookingID)
+	REFERENCES Bookings(PK_BookingID);
+
+ALTER TABLE SitterApplications
+	ADD CONSTRAINT fk_UserIDReference
+	FOREIGN KEY (FK_UserID)
+	REFERENCES Users(PK_UserID);
