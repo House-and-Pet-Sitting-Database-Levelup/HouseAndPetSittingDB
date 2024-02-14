defee29e07199265de72553085581da0ae493da5
RotendaBBD <Rotenda.Mantsha@bbd.co.za>
2024-02-14 08:59:48 +0200

    Fixing fields names and types

--- flyway/migrations/V5__Create_SittingJobTable.sql
+++ flyway/migrations/V5__Create_SittingJobTable.sql
@@ -1,13 +1,15 @@
-CREATE TABLE SittingJob (
-    PK_jobID uniqueidentifier DEFAULT NEWSEQUENTIALID() PRIMARY KEY,
+CREATE TABLE Bookings (
+    PK_bookingID uniqueidentifier DEFAULT NEWSEQUENTIALID() PRIMARY KEY,
     FK_sitterID uniqueidentifier NOT NULL,
     FK_clientID uniqueidentifier NOT NULL,
-    startDate DATE,
-    startTime TIME,
-    endDate DATE,
-    endTime TIME,
-    jobRate INT,
+    FK_serviceTypeiD uniqueidentifier,
+    startDateTime DATETIME NOT NULL,
+    endDateTime DATETIME NOT NULL,
+    additionalNotes VARCHAR (MAX),
+    totalCost DECIMAL (18, 2) NOT NULL,
+    status VARCHAR (255),
 
     FOREIGN KEY (FK_sitterID) REFERENCES Sitters(PK_sitterID),
-    FOREIGN KEY (FK_clientID) REFERENCES Users(PK_UserID)
+    FOREIGN KEY (FK_clientID) REFERENCES Users(PK_UserID),
+    FOREIGN KEY (FK_serviceTypeID) REFERENCES ServiceTypes(PK_serviceTypeID)
 )
