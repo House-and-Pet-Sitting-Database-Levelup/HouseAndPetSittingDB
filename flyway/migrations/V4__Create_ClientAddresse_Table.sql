cef645e06c709dfcfec2cb4486bec1c8cd729af3
RotendaBBD <Rotenda.Mantsha@bbd.co.za>
2024-02-14 08:50:03 +0200

    fixing field names

--- flyway/migrations/V4__Create_ClientAddresse_Table.sql
+++ flyway/migrations/V4__Create_ClientAddresse_Table.sql
@@ -1,10 +1,11 @@
 CREATE TABLE ClientAddresses(
     PK_addressID uniqueidentifier PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
-    FK_clientID uniqueidentifier NOT NULL
-    addressLine1 varchar(225) NOT NULL
-    addressLine2 varchar(225) NOT NULL
-    city varchar(225) NOT NULL
-    suburb varchar (225) NOT NULL
-    postalCode varchar (225) NOT NULL
-    FOREIGN KEY (FK_clientID) REFERENCE Users(PK_UserID)
+    FK_UserID uniqueidentifier NOT NULL,
+    streetAddress varchar(225) NOT NULL,
+    city varchar(225) NOT NULL,
+    state varchar (225) NOT NULL,
+    postalCode varchar (225) NOT NULL,
+    isPrimary BOOLEAN,
+    
+    FOREIGN KEY (FK_UserID) REFERENCE Users(PK_UserID)
 )
