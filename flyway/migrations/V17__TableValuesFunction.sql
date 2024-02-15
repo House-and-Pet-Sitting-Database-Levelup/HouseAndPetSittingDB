3c130a3926d2d687812f07a219f57e1008e352eb
RotendaBBD <159693321+RotendaBBD@users.noreply.github.com>
2024-02-15 08:39:40 +0200

    Update V17__TableValuesFunction.sql syntax

--- flyway/migrations/V17__TableValuesFunction.sql
+++ flyway/migrations/V17__TableValuesFunction.sql
@@ -1,13 +1,33 @@
-CREATE FUNCTION GetSitterApplicationByStatus
-(
-    @status VARCHAR(225)
-)
+DELIMITER //
 
-RETURNS TABLE
-AS
-RETURNS
-(
-    SELECT *
-    FROM SitterApplications
-    WHERE applicationStatus = @status
-)
+CREATE PROCEDURE ApproveSitterApplication (IN applicationID INT)
+BEGIN  
+    DECLARE exit handler for sqlexception
+    BEGIN
+        ROLLBACK;
+        RESIGNAL;
+    END;
+
+    DECLARE exit handler for sqlwarning
+    BEGIN
+        ROLLBACK;
+        RESIGNAL;
+    END;
+
+    START TRANSACTION;
+    
+    UPDATE SitterApplications
+    SET applicationStatus = 'Approved'
+    WHERE PK_ApplicationID = applicationID
+        AND applicationStatus = 'Pending';
+    
+    IF ROW_COUNT() > 0 THEN
+        SELECT 'Sitter Application approved';
+    ELSE
+        SELECT 'Sitter application ID not found, already approved, or no changes made';
+    END IF;
+
+    COMMIT;
+END //
+
+DELIMITER ;
