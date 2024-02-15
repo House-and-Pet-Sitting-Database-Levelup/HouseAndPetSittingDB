7069926e83e82602d27544330482ba20e90d2374
RotendaBBD <159693321+RotendaBBD@users.noreply.github.com>
2024-02-15 08:41:07 +0200

    Update V16__ApproveSitterApplicationProc.sql syntax

--- flyway/migrations/V16__ApproveSitterApplicationProc.sql
+++ flyway/migrations/V16__ApproveSitterApplicationProc.sql
@@ -1,19 +1,33 @@
-CREATE PROCEDURE ApproveSitterApplication
-    @applicationID uniqueidentifier
+DELIMITER //
 
-AS
+CREATE PROCEDURE ApproveSitterApplication (IN applicationID INT)
 BEGIN  
+    DECLARE exit handler for sqlexception
+    BEGIN
+        ROLLBACK;
+        RESIGNAL;
+    END;
+
+    DECLARE exit handler for sqlwarning
+    BEGIN
+        ROLLBACK;
+        RESIGNAL;
+    END;
+
+    START TRANSACTION;
+    
     UPDATE SitterApplications
     SET applicationStatus = 'Approved'
-    WHERE PK_ApplicationID = @applicationID
+    WHERE PK_ApplicationID = applicationID
         AND applicationStatus = 'Pending';
     
-    IF @@ROWCOUNT > 0
-    BEGIN  
-        PRINT 'Sitter Application approved';
-    END
+    IF ROW_COUNT() > 0 THEN
+        SELECT 'Sitter Application approved';
     ELSE
-    BEGIN  
-        PRINT 'Sitter application ID not found, already approved, or no changes made';
-    END
-END
+        SELECT 'Sitter application ID not found, already approved, or no changes made';
+    END IF;
+
+    COMMIT;
+END //
+
+DELIMITER ;
