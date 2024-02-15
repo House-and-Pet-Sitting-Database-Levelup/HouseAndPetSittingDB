f49db0b9f428c43ff3aec87a0ad9d927c7d7f13c
Work Account <workaccount@Timos-MacBook-Air.local>
2024-02-15 08:59:11 +0200

    removing pipeline test

--- flyway/migrations/V16__TestTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    user_id INT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash VARCHAR(100) NOT NULL,
-    registration_date DATE,
-    last_login_date DATE
-);
