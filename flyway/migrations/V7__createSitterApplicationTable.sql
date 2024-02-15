936687cdbd391d62acceddc9aad014f5a0907f8d
jennadunford <jenna.dunford@bbd.co.za>
2024-02-15 13:40:21 +0200

    Update V7__createSitterApplicationTable.sql

--- flyway/migrations/V7__createSitterApplicationTable.sql
+++ flyway/migrations/V7__createSitterApplicationTable.sql
@@ -2,8 +2,8 @@ CREATE TABLE SitterApplications(
 	PK_ApplicationID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	FK_BookingID int NOT NULL,
 	FK_UserID int NOT NULL,
-	applicationStatus varchar(225),
-	--CONSTRAINT chk_Status CHECK (applicationStatus IN ('Pending','Approved', 'Rejected'))-
+	applicationStatus varchar(225)
+	-- CONSTRAINT chk_Status CHECK (applicationStatus IN ('Pending','Approved', 'Rejected'))-
 );
 
 ALTER TABLE SitterApplications
