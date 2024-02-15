1491fc206f3b45de12e1a471f6bb6931aa2d6a89
jennadunford <jenna.dunford@bbd.co.za>
2024-02-15 13:18:36 +0200

    Update V15__TriggerForUpdateSitterApplicationStatus.sql

--- flyway/migrations/V15__TriggerForUpdateSitterApplicationStatus.sql
+++ flyway/migrations/V15__TriggerForUpdateSitterApplicationStatus.sql
@@ -6,7 +6,8 @@ FOR EACH ROW
 BEGIN
     IF NEW.BookingStatus = 'Approved' THEN
 		UPDATE SitterApplications SET applicationStatus = 'Approved' WHERE FK_SitterID = NEW.FK_SitterID AND FK_BookingID = NEW.PK_BookingID;
-		UPDATE SitterApplications SET applicationStatus = 'Rejected' WHERE FK_SitterID != NEW.FK_SitterID AND FK_BookingID = NEW.PK_BookingID;
+		UPDATE SitterApplications SET applicationStatus = 'Rejected' WHERE FK_SitterID <> NEW.FK_SitterID AND FK_BookingID = NEW.PK_BookingID;
+		SELECT 'Should have set approved and rejected' AS output_message;
     END IF;
 	IF NEW.BookingStatus = 'Cancelled' THEN
 		DELETE FROM SitterApplications WHERE FK_BookingID = NEW.PK_BookingID;
