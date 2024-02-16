7a330bd8d05d54077c720bcc250ac6a5118a9de8
jennadunford <jenna.dunford@bbd.co.za>
2024-02-16 13:13:17 +0200

    Fixes

--- flyway/migrations/V2__ServiceTypesTable.sql
+++ flyway/migrations/V2__ServiceTypesTable.sql
@@ -2,4 +2,17 @@ CREATE TABLE ServiceTypes (
     PK_serviceTypeID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     description VARCHAR(255),
     hourlyRate DECIMAL(10, 2) NOT NULL
-)
+);
+
+CREATE TABLE StatusTypes (
+   PK_statusType int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   description VARCHAR(255) NOT NULL
+);
+
+INSERT INTO StatusTypes (description) 
+VALUES
+('Approved'),
+('Rejected'),
+('Pending'),
+('Completed'),
+('Canceled');
